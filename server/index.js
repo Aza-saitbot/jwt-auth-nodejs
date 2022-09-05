35749507edb7bc0068b122e5d5e48eaa345dae07
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-09-05 22:27:08 +0300

    fix/ connected mongodb

--- server/index.js
+++ server/index.js
@@ -2,17 +2,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import cors from 'cors'
-import cookieParser from 'cookie-parse'
+import cookieParser from 'cookie-parser'
 import mongoose from 'mongoose'
 
 const PORT=process.env.PORT || 5000
 const app=express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
 
 const start = async () => {
     try {
         await mongoose.connect(process.env.DB_URL)
         app.listen(PORT,()=>console.log(`Server started on port = ${PORT}`))
-
     }catch (e) {
         console.log('ошибка при запуске сервера',e)
     }
